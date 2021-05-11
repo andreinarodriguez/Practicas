ba59b3493e8a9e11a12c3e06f2cea8ed557507ca
Andreina <Andreina@192.168.0.52>
2021-05-10 21:06:07 -0300

    Project

---
+++ AplicationChallenge/src/main/java/com/challenge/controller/AplicationChallengeApplication.java
@@ -0,0 +1,15 @@
+package com.challenge.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AplicationChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicationChallengeApplication.class, args);
+		
+	}
+
+}
